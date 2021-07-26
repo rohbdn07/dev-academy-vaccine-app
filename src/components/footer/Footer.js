77d2df5b5327fa48c463b612583746d220a6e9cb
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-07-26 16:18:40 +0300

    footer section added

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaCcMastercard, FaCopyright} from 'react-icons/fa';
+import { GiLoveInjection } from 'react-icons/gi';
+
+import '../style.css';
+
+export default function Footer() {
+    return (
+        <>
+            <div className="footer_end ">
+                <div className="fotend_wrapper col-lg-10 mx-auto">
+                    <div className="fotend_left">
+                        <p className="text-light">2021 Dev <GiLoveInjection /> vaccine</p>
+                    </div>
+                    <div className="fotend_middle">
+                        <p><FaCopyright /> rohit.raj.bhandari07@gmail.com</p>
+                        <p>Helsinki-Finland</p>
+                    </div>
+                    <div className="fotend_right">
+                        <a href="https://github.com/rohbdn07/dev-academy-vaccine-app" target="_blank"><FaGithub className="mx-1 text-light" /></a>
+                        <a href="https://www.linkedin.com/in/rohit-bhandari-5b4994177/" target="_blank"><FaLinkedin className="mx-2 text-light" /></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
