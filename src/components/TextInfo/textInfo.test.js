5efcfbb568b63a55b663a06f71471f4c8a49b57b
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-07-26 18:03:13 +0300

    added more tests

--- src/components/TextInfo/textInfo.test.js
+++ src/components/TextInfo/textInfo.test.js
@@ -8,6 +8,12 @@ describe('textInfo component', () => {
         const wrapper = shallow(<TextInfo />);
         expect(wrapper.find('.textInfo')).toHaveLength(1)
     })
-
-   
+    it('should contain vaccineName passed as a props', () => {
+        const wrapper = shallow(<TextInfo />);
+        expect(wrapper.find('VaccineName')).toBeTruthy()
+    })
+    it('should contain kysArray passed as a props', () => {
+        const wrapper = shallow(<TextInfo />);
+        expect(wrapper.find('kysArray')).toBeTruthy()
+    })
 })
