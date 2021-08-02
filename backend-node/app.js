9fda67c1ef06a52b984a4d9b127a61e97cfc91c8
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-02 23:09:17 +0300

    nodejs for backend is init

---
+++ backend-node/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// enable cors
+app.use(cors());
+
+module.exports = app;
