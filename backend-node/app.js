27e1c46fd8bcb02ef8f1b113739f9979124131f7
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-03 00:15:21 +0300

    connection to db successful

--- backend-node/app.js
+++ backend-node/app.js
@@ -1,15 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mangoose= require('mongoose');
+
 
 const app = express();
 
-// parse json request body
-app.use(express.json());
 
+//connection to Mangodb...
+const dbURI = process.env.MONGODB_URI;
+mangoose.connect( dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log("there is an error", err));
+
+
+//listing to LocalHost
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`listening on ${PORT}`));
+  console.log('  Press CTRL-C to stop\n');
+
+
+  // parse json request body
+app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
-
 // enable cors
 app.use(cors());
+
+app.get('/', (req,res) => {
+    res.send('I am coding late night, feeling sleepy')
+})
+
 
 module.exports = app;
