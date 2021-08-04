40d8e2ce5125576919214a747be40b64c468d03a
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-05 02:10:53 +0300

    comment removed

--- backend-node/router/getVaccine.js
+++ backend-node/router/getVaccine.js
@@ -9,28 +9,14 @@ router.get("/", async (req, res) => {
     const receivedallVaccinationdata = await AllVaccinationData.find({});
 
     // console.log('the data are', res.json(antqueData))
-    res
-      .status(200)
-      .json({
-        allCompanyVaccineData: receivedalldata,
-        allVaccinationDoneData: receivedallVaccinationdata,
-      });
+    res.status(200).json({
+      allCompanyVaccineData: receivedalldata,
+      allVaccinationDoneData: receivedallVaccinationdata,
+    });
   } catch (error) {
     console.log("there is an error getting data", error);
     res.status(400).json({ message: "unable to get data" });
   }
 });
-
-// router.get('/vaccination-done', async(req,res) => {
-//     try {
-//         const receivedalldata = await AllVaccinationData.find({});
-
-//         // console.log('the data are', res.json(antqueData))
-//         res.status(200).json(receivedalldata);
-//     } catch (error) {
-//         console.log('there is an error getting data', error);
-//         res.status(400).json({message:'unable to get vaccination done data'})
-//     }
-// })
 
 module.exports = router;
