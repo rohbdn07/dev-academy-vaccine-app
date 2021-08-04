fb598d195482a3398c08e3d7b2e6c368091f019d
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-05 00:06:41 +0300

    commented codes are removed

--- client-react/src/Redux/Action/getData-action.js
+++ client-react/src/Redux/Action/getData-action.js
@@ -24,30 +24,3 @@ export const getDataAction = () => async (dispatch) => {
     console.log("there is an error", error);
   }
 };
-
-// export const getSolarBuddhicaAction = (SolarBuddhica) => {
-//     return (
-//         {
-//             type: GET_SOLARBUDDHICA_DATA,
-//             payload:SolarBuddhica
-//         }
-//     )
-// }
-
-// export const getZerpfyAction = (zerpfy) => {
-//     return (
-//         {
-//             type: GET_ZERPFY_DATA,
-//             payload:zerpfy
-//         }
-//     )
-// }
-
-// export const getVaccinationAction = (vaccination) => {
-//     return (
-//         {
-//             type: VACCINATION_DONE,
-//             payload:vaccination
-//         }
-//     )
-// }
