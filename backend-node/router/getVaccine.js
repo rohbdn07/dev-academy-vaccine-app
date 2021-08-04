2dc3e2a2c79264ac8d868b3ad12083f29ef1aebf
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-04 23:51:17 +0300

    sent the data to front-end successfully

--- backend-node/router/getVaccine.js
+++ backend-node/router/getVaccine.js
@@ -1,32 +1,36 @@
-const express = require('express')
-const router = express.Router()
-const AllVaccineCompanyData = require('../model/vaccineCompany-schema')
-const AllVaccinationData = require('../model/vaccinationDone-schema')
+const express = require("express");
+const router = express.Router();
+const AllVaccineCompanyData = require("../model/vaccineCompany-schema");
+const AllVaccinationData = require("../model/vaccinationDone-schema");
 
+router.get("/", async (req, res) => {
+  try {
+    const receivedalldata = await AllVaccineCompanyData.find({});
+    const receivedallVaccinationdata = await AllVaccinationData.find({});
 
-router.get('/', async(req,res) => {
-    try {
-        const receivedalldata = await AllVaccineCompanyData.find({});
+    // console.log('the data are', res.json(antqueData))
+    res
+      .status(200)
+      .json({
+        allCompanyVaccineData: receivedalldata,
+        allVaccinationDoneData: receivedallVaccinationdata,
+      });
+  } catch (error) {
+    console.log("there is an error getting data", error);
+    res.status(400).json({ message: "unable to get data" });
+  }
+});
 
-        // console.log('the data are', res.json(antqueData))
-        res.status(200).send(receivedalldata);
-    } catch (error) {
-        console.log('there is an error getting data', error);
-        res.status(400).send({message:'unable to get data'})
-    }
-})
+// router.get('/vaccination-done', async(req,res) => {
+//     try {
+//         const receivedalldata = await AllVaccinationData.find({});
 
-router.get('/vaccination-done', async(req,res) => {
-    try {
-        const receivedalldata = await AllVaccinationData.find({});
-
-        // console.log('the data are', res.json(antqueData))
-        res.status(200).send(receivedalldata);
-    } catch (error) {
-        console.log('there is an error getting data', error);
-        res.status(400).send({message:'unable to get vaccination done data'})
-    }
-})
-
+//         // console.log('the data are', res.json(antqueData))
+//         res.status(200).json(receivedalldata);
+//     } catch (error) {
+//         console.log('there is an error getting data', error);
+//         res.status(400).json({message:'unable to get vaccination done data'})
+//     }
+// })
 
 module.exports = router;
