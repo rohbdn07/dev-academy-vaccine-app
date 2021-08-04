49a8f5a8d4f5feccc685ad4804f07bb8fab6bee9
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-04 23:47:10 +0300

    code is re-factorised and added single component for result instead of many

---
+++ client-react/src/components/results/VaccineResults.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import BarChart_producers from "../BarChart_producers";
+import TextInfo from "../TextInfo/TextInfo";
+
+export default function VaccineResults(props) {
+  const {
+    receivedVaccineArray,
+    VaccineName,
+    dataLength,
+    kysArray,
+    HyksArray,
+    TyksArray,
+    OysArray,
+    TaysArray,
+  } = props;
+
+  return (
+    <>
+      <div className="col-12 vaccinationResult">
+        <TextInfo
+          receivedVaccineArray={receivedVaccineArray.length}
+          VaccineName={VaccineName}
+          dataLength={dataLength}
+          kysArray={kysArray.length}
+          HyksArray={HyksArray.length}
+          TyksArray={TyksArray.length}
+          OysArray={OysArray.length}
+          TaysArray={TaysArray.length}
+        />
+        <BarChart_producers
+          kysArray={kysArray.length}
+          HyksArray={HyksArray.length}
+          TyksArray={TyksArray.length}
+          OysArray={OysArray.length}
+          TaysArray={TaysArray.length}
+        />
+      </div>
+    </>
+  );
+}
