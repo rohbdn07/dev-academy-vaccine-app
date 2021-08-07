23ffcbb38d8b0d8b2486464377df9063717299fc
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-08 00:53:28 +0300

    PORT number set to 5000

--- client-react/src/services/axios.js
+++ client-react/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL:'http://localhost:5050',
+  baseURL:'http://localhost:5000',
   headers: {
     'Content-type': 'application/json'
   }});
