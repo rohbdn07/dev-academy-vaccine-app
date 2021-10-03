e13e51f74179689676665242687f9863a225ec98
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-10-03 19:11:51 +0300

    path to build folder is corrected

--- backend-node/app.js
+++ backend-node/app.js
@@ -11,16 +11,16 @@ const app = express();
 //connection to Mangodb...
 // const dbURI = process.env.MONGODB_URI;
 mangoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvvz3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }
-  )
-  .then(() => console.log("connected to mongo-db"))
-  .catch((err) => console.log("there is an error", err));
+    .connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvvz3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        }
+    )
+    .then(() => console.log("connected to mongo-db"))
+    .catch((err) => console.log("there is an error", err));
 
 //listing to LocalHost
 const PORT = process.env.PORT || 5000;
@@ -34,7 +34,15 @@ app.use(express.urlencoded({ extended: true }));
 // enable cors
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+//Serve static assists if in PRODUCTION
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client-react", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.join(__dirname, "../client-react", "build", "index.html")
+        );
+    });
+}
 
 //get data from db
 app.use(getvaccineRouter);
@@ -43,7 +51,7 @@ app.use(getvaccineRouter);
 // When an HTTP client receives a response with status 302, it will send
 // an HTTP request to the URL in the response, in this case `/`
 router.get("*", (req, res) => {
-  res.redirect("/");
+    res.redirect("/");
 });
 
 module.exports = app;
