65a0c4754be0d107515c9ce954419cbda4f21d78
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-10-03 23:42:28 +0300

    api end-point changed

--- backend-node/router/getVaccine.js
+++ backend-node/router/getVaccine.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const getVaccineController = require("../controllers/getvaccineController");
 
-
 //GET: route(/) to get all the data from db.
-router.get("/", getVaccineController.vaccine_index_get);
+router.get("/api/vaccines", getVaccineController.vaccine_index_get);
 
 module.exports = router;
